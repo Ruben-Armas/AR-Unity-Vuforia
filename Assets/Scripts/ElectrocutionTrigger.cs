e0478a6cf43e52ce642fa307aa69d01dfc09cf72
Ruben-Armas <rubenarmas2010@gmail.com>
2023-11-16 19:25:50 +0000

    Adding Scripts

---
+++ Assets/Scripts/ElectrocutionTrigger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectrocutionTrigger : MonoBehaviour {
+    public ParticleSystem electrocutionParticles;
+
+    void Start() {
+
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision != null) {
+            if (collision.gameObject.CompareTag("Wall")) {
+                StartElectrocutionEffect();
+            }
+        }
+    }
+
+    private void StartElectrocutionEffect() {
+        electrocutionParticles.Play();
+    }
+}
